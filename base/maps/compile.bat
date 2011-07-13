938d0f9d92d33e39651807645cd1abe3a8eccde4
Andrew Browne <dersaidin@dersaidin.net>
2011-07-13 21:06:25 +1000

    Tweak lighting options for bow_block.

--- base/maps/compile.bat
+++ base/maps/compile.bat
@@ -10,7 +10,7 @@ SET starttime=%time%
 "%xmap2%" -fs_basepath "%basepath%" -fs_game "base" -game "xreal" -meta -v -leaktest "%1.map"
 "%xmap2%" -fs_basepath "%basepath%" -fs_game "base" -game "xreal" -vis -v "%1.bsp"
 @REM "%xmap2%" -fs_basepath "%basepath%" -fs_game "base" -game "xreal" -light -v -fast -samples 2 -lightmapsize 1024 -lomem "%1.map"
-"%xmap2%" -fs_basepath "%basepath%" -fs_game "base" -game "xreal" -light -v -fast -samples 2 -scale 1.5 -pointscale 1.5 -gamma 1.3 -bounce 8 -lightmapsize 1024 -compensate 4 -dirty "%1.map"
+"%xmap2%" -fs_basepath "%basepath%" -fs_game "base" -game "xreal" -light -v -fast -samples 2 -scale 1.6 -pointscale 1.8 -areascale 1.6 -gamma 1.35 -bounce 8 -lightmapsize 1024 -compensate 4 -dirty -dirtscale 0.4 -dirtdepth 64 "%1.map"
 
 echo %starttime%
 echo %time%
