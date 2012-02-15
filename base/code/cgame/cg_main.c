cf86e2d3f6a23c4eaa9c7eecb87bbcd35e491126
Andrew Browne <dersaidin@dersaidin.net>
2012-02-15 21:53:02 +1000

    Set cg_drawTimer 1 by default.

--- base/code/cgame/cg_main.c
+++ base/code/cgame/cg_main.c
@@ -286,7 +286,7 @@ static cvarTable_t cvarTable[] = {	// bk001129
 	{&cg_drawSideBar, "cg_drawSideBar", "0", CVAR_ARCHIVE},
 	{&cg_drawPickupItem, "cg_drawPickupItem", "1", CVAR_ARCHIVE},
 	{&cg_drawWeaponSelect, "cg_drawWeaponSelect", "1", CVAR_ARCHIVE},
-	{&cg_drawTimer, "cg_drawTimer", "0", CVAR_ARCHIVE},
+	{&cg_drawTimer, "cg_drawTimer", "1", CVAR_ARCHIVE},
 	{&cg_drawFPS, "cg_drawFPS", "0", CVAR_ARCHIVE},
 	{&cg_drawSnapshot, "cg_drawSnapshot", "0", CVAR_ARCHIVE},
 	{&cg_draw3dIcons, "cg_draw3dIcons", "0", CVAR_ARCHIVE},
