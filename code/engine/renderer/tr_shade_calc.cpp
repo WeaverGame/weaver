ae74aa9c5370861bb6b12168359e2f902b834762
Andrew Browne <dersaidin@dersaidin.net>
2012-09-01 00:25:07 +1000

    Updated license of renderer files to GPLv3 or later. XreaL Renderer was licensed GPLv2 or later. From cross polination with Wolf:ET (probably doom3 later on) we have a decent bit of GPLv3 code. GPLv3 terms forces all the GPLv2 or later code to be taken as GPLv3 or later.

--- code/engine/renderer/tr_shade_calc.cpp
+++ code/engine/renderer/tr_shade_calc.cpp
@@ -7,7 +7,7 @@ This file is part of XreaL source code.
 
 XreaL source code is free software; you can redistribute it
 and/or modify it under the terms of the GNU General Public License as
-published by the Free Software Foundation; either version 2 of the License,
+published by the Free Software Foundation; either version 3 of the License,
 or (at your option) any later version.
 
 XreaL source code is distributed in the hope that it will be
